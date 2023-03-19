a10cbc56770ce6c1a955c2a7e76ade154af8b214
RohakDev <45899405+ImRohak@users.noreply.github.com>
2023-03-19 21:48:03 +0000

    Created test auth setup on backend for now.

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,9 +4,9 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import { env } from "~/env.mjs";
-
+import CredentialsProvider from "next-auth/providers/credentials";
+import type { User } from "next-auth";
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
  * object and keep type safety.
@@ -44,28 +44,37 @@ export const authOptions: NextAuthOptions = {
     },
   },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "username", type: "text", placeholder: "Username..." },
+        password: { label: "password", type: "password" }
+      },
+      async authorize(credentials) {
+
+        type Payload = {
+          username: string | undefined,
+          password: string | undefined
+        };
+        
+        const payload: Payload = {
+          username: credentials?.username,
+          password: credentials?.password
+        };
+
+        if (payload.username === "Rohak" && payload.password === "Rohak123") {
+          const user: object = { "id": "hello", "username": "Rohak", "password": "Rohak123" };
+          return user;
+        }
+
+        return null;
+      }
+    })
   ],
+  secret: env.NEXTAUTH_JWT_SECRET,
 };
 
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
-export const getServerAuthSession = (ctx: {
+export const getBranchOutServerSession = (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
